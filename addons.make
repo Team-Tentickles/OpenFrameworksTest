8114684a253c48864aa847462b19d9175ddbc3c2
Ben W <reccanti@gmail.com>
2016-03-10 23:24:18 -0500

    Initial Commit

---
+++ addons.make
@@ -0,0 +1,2 @@
+ofxSimpleHttp
+ofxSocketIO
---
+++ src/ofApp.h
@@ -0,0 +1,27 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxSimpleHttp.h"
+
+#define OUTPUT_DIRECTORY "tempDownloads"
+
+class ofApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    void exit();
+    
+    void keyPressed(int key);
+    
+    float p1;
+    bool timeSample;
+    
+    void newResponse(ofxSimpleHttpResponse &response);
+    
+    ofxSimpleHttp http;
+    ofImage img;
+    vector<string> downloadList;
+    
+};
